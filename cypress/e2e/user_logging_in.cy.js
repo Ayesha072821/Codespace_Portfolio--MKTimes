5fb763d040a9a6f9df139a8c3b19caf7c1eb8564
Ayesha072821 <ayeshy19@gmail.com>
2025-01-15 10:26:38 +0000

    Testing Website After logging in

--- cypress/e2e/user_logging_in.cy.js
+++ cypress/e2e/user_logging_in.cy.js
@@ -22,8 +22,63 @@ describe('Loading MKTimes Website and try to log in with wrong credentials', () 
     // if user logs in successfully it will take to index page
 
     cy.url().should('be.equal','http://localhost/Codespace_Portfolio--MKTimes--EC2284074/index.php')
+
+
+
+    // tests to see if website behaves properly after logging in.
+
+    // if user is logged in it should give access to all pages 
+
+    // test to check if user can view Female collection.
+
+    cy.get('#her').click()
+
+    //url should be female collection page
+
+    cy.url().should('be.equal','http://localhost/Codespace_Portfolio--MKTimes--EC2284074/femalecollection.php')
+
+
+
+    //click on MK Time and it should take user to the index page
+
+    cy.get('.navbar-brand').click()
+ 
+    //user should go to index page
+
+    cy.url().should('be.equal','http://localhost/Codespace_Portfolio--MKTimes--EC2284074/index.php')
+
+    // now we will view Male collection from index page
+
+    cy.get('#him').click()
+
+    // user should be able to see Male collection
+
+    cy.url().should('be.equal','http://localhost/Codespace_Portfolio--MKTimes--EC2284074/malecollection.php')
+
+    //now user should be able to click of OUR Collection button on header
+
+    cy.get('#navbarDarkDropdownMenuLink').click()
+
+    //user selects the Female collection 
+    cy.get('#herfromheader').click()
+    
+    //user should go to female collection page
+
+     cy.url().should('be.equal','http://localhost/Codespace_Portfolio--MKTimes--EC2284074/femalecollection.php')
     
     
+    //now user should be able to click of OUR Collection button on header
+
+    cy.get('#navbarDarkDropdownMenuLink').click()
+
+    //user selects the Male collection 
+    cy.get('#himfromheader').click()
+    
+    //user should go to Male collection page
+
+    cy.url().should('be.equal','http://localhost/Codespace_Portfolio--MKTimes--EC2284074/malecollection.php')
+    
+      
   
   })
 })
